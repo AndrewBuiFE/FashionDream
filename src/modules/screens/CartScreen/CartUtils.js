731eb31c7a9b99d47a3da73c1837c64c6be4ebe6
buiviethoang <buiviethoang12062000@gmail.com>
2022-06-13 10:20:47 +0700

    set up data

---
+++ src/modules/screens/CartScreen/CartUtils.js
@@ -0,0 +1,12 @@
+class CartUtils {
+    constructor() {}
+    async saveCartData(cartData) {
+        try {
+            const stringifyData = JSON.stringify(cartData)
+            await setPrefer
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
